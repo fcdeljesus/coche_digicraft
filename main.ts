d95c6d0c349b3a1b806d4c77c83d56d91863ebe9
Francisco Javier Carmona del Jesus <98518154+fcdeljesus@users.noreply.github.com>
2023-04-06 12:18:06 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,50 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 150) {
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
-        	
-        }
+        pixel = 1
     }
 })
+input.onButtonPressed(Button.A, function () {
+    pixel = 1
+})
+function parar () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(5000)
+}
+function andar () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    } else {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorStop(maqueen.Motors.All)
+            parar()
+        }
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+        } else {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            }
+        }
+    }
+}
+let strip: neopixel.Strip = null
+let pixel = 0
+pixel = 0
 radio.setGroup(200)
 basic.showString("Explorar")
-let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
-    basic.showIcon(IconNames.Happy)
-    basic.pause(500)
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.showIcon(IconNames.Asleep)
-    basic.pause(500)
-    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+    if (pixel == 0) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.showIcon(IconNames.Asleep)
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+    } else {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        andar()
+    }
 })
